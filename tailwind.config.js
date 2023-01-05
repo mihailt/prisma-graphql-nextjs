9957671474acce4391a030490d20da0c8b34edc3
Mihails Tumkins <mihailt@gmail.com>
2023-01-05 16:06:13 +0200

    add tailwindcss configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const typography = require('@tailwindcss/typography');
+const forms = require('@tailwindcss/forms');
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography, forms],
+};
