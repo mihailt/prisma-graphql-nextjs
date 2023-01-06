44c71eadee1a184624817cb2813a9c0f5f25b018
Mihails Tumkins <mihailt@gmail.com>
2023-01-06 12:33:59 +0200

    add cursor based pagination for posts

--- graphql/resolvers/Posts/index.ts
+++ graphql/resolvers/Posts/index.ts
@@ -2,15 +2,16 @@ import { Context } from '../../context';
 
 export interface PostsArgs {
   limit: number
-  offset: number
+  cursor?: string
 }
 
 const resolvers = {
   Query: {
     posts: async (_parent: undefined, args: PostsArgs, ctx: Context) => {
       const posts = await ctx.prisma.post.findMany({
-        skip: args.offset,
+        skip: args.cursor ? 1 : 0,
         take: args.limit,
+        cursor: args.cursor ? { id: args.cursor } : undefined,
         orderBy: {
           id: 'desc',
         },
