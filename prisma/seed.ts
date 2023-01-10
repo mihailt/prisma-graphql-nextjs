0a9b78d8296410f3b857a6af75f37b39de1183fa
Mihails Tumkins <mihailt@gmail.com>
2023-01-10 12:15:51 +0200

    add shema changes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,72 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-const posts = Array(100).fill(
+const files = [
   {
-    description: 'GraphQL implementation ',
-    imageUrl: 'https://www.apollographql.com/apollo-home.jpg',
-    title: 'Apollo GraphQL',
-    url: 'https://apollographql.com',
-    userId: '',
+    id: '8a9020b2-363b-4a4f-ad26-d6d55b51bqe5',
+    url: 'https://s3.eu-north-1.amazonaws.com/experimental-popspot/01977a11-b6c9-4ba5-b01b-c67730612c9eDonut1.m4v',
   },
-);
+  {
+    id: '8a9020b2-363b-4a4f-ad26-d6d55b51bqe4',
+    url: 'https://nextjs.org/static/twitter-cards/home.jpg',
+  },
+  {
+    id: '8a9020b2-363b-4a4f-ad26-d6d55b51bqe3',
+    url: 'https://www.prisma.io/images/og-image.png',
+  },
+  {
+    id: '8a9020b2-363b-4a4f-ad26-d6d55b51bqe2',
+    url: 'https://tailwindcss.com/_next/static/media/social-card-large.f6878fd8df804f73ba3f1a271122105a.jpg',
+  },
+  {
+    id: '8a9020b2-363b-4a4f-ad26-d6d55b51bqe1',
+    url: 'https://www.apollographql.com/apollo-home.jpg',
+  },
+];
+
+const posts = [
+  {
+    id: '8a9020b2-363b-4a4f-ad26-d6d55b51bqe',
+    title: 'Next.js',
+    description: 'Fullstack React framework',
+    imageId: '8a9020b2-363b-4a4f-ad26-d6d55b51bqe4',
+  },
+  {
+    id: '2a3121b2-363b-4a4f-ad26-d6c35b41bade',
+    title: 'Prisma',
+    description: 'Next Generation ORM for TypeScript and JavaScript',
+    imageId: '8a9020b2-363b-4a4f-ad26-d6d55b51bqe3',
+  },
+  {
+    id: '6a9122b2-363b-4a4f-ad26-d6c55b51baed',
+    title: 'TailwindCSS',
+    description: 'Utility-fist css framework',
+    imageId: '8a9020b2-363b-4a4f-ad26-d6d55b51bqe2',
+  },
+  {
+    id: '2ea8cfb0-44a3-4c07-bdc2-31ffa135ea78',
+    title: 'Apollo GraphQL',
+    description: 'GraphQL implementation ',
+    imageId: '8a9020b2-363b-4a4f-ad26-d6d55b51bqe1',
+  },
+];
 
 async function main() {
-  const user = await prisma.user.create({
+  await prisma.user.create({
     data: {
+      id: '8a9020b2-363b-4a4f-ad26-d6d55b51b444',
       email: 'mihailt@gmail.com',
+      files: {
+        createMany: {
+          data: files,
+        },
+      },
+      posts: {
+        createMany: {
+          data: posts,
+        },
+      },
     },
-  });
-
-  posts.forEach((post) => {
-    post.userId = user.id;
-  });
-
-  await prisma.post.createMany({
-    data: posts,
   });
 }
 
