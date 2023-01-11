8c6d14898e12b3d6746394ca65f84ed7de68e751
Mihails Tumkins <mihailt@gmail.com>
2023-01-11 13:12:24 +0200

    change aws env names

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -13,20 +13,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     const s3 = new aws.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
-      region: process.env.AWS_REGION,
+      accessKeyId: process.env.APP_AWS_ACCESS_KEY,
+      secretAccessKey: process.env.APP_AWS_SECRET_KEY,
+      region: process.env.APP_AWS_REGION,
     });
 
     aws.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
-      region: process.env.AWS_REGION,
+      accessKeyId: process.env.APP_AWS_ACCESS_KEY,
+      secretAccessKey: process.env.APP_AWS_SECRET_KEY,
+      region: process.env.APP_AWS_REGION,
       signatureVersion: 'v4',
     });
 
     const post = await s3.createPresignedPost({
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Bucket: process.env.APP_AWS_S3_BUCKET_NAME,
       Fields: {
         key: `${req.query.file}`,
       },
